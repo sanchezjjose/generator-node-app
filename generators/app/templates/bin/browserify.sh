1d08884504c7fdd8d614c8eb3bfb54660d9da7ef
josanche <jose.sanchez@mlb.com>
2016-09-14 00:20:14 -0400

    bundling fixes

--- generators/app/templates/bin/browserify.sh
+++ generators/app/templates/bin/browserify.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-(browserify lib/components/dashboard/overview.js | node_modules/uglify-js/bin/uglifyjs --compress --mangle --screw-ie8 > public/javascripts/overview.bundle.js)
-(browserify lib/securityGroups.js -s SecurityGroupsComponent | node_modules/uglify-js/bin/uglifyjs --compress --mangle > public/javascripts/securityGroups.bundle.js)
-(browserify lib/tags.js -s TagsComponent | node_modules/uglify-js/bin/uglifyjs --compress --mangle > public/javascripts/tags.bundle.js)
+(browserify lib/index.js -s HelloWorldComponent | node_modules/uglify-js/bin/uglifyjs --compress --mangle > public/javascripts/index.bundle.js)
--- generators/app/templates/bin/watchify.sh
+++ generators/app/templates/bin/watchify.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-(watchify lib/components/dashboard/overview.js -o public/javascripts/overview.bundle.js -dv) &
-(watchify lib/securityGroups.js -s SecurityGroupsComponent -o public/javascripts/securityGroups.bundle.js -dv) &
-(watchify lib/tags.js -s TagsComponent -o public/javascripts/tags.bundle.js -dv)
+(watchify lib/index.js -s HelloWorldComponent -o public/javascripts/index.bundle.js -dv)
